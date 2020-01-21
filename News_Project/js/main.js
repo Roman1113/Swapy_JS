56ca7448eb49b1d0bed89253fec1b73df3b61f21
maksutroman <maksutroman@gmail.com>
2020-01-21 14:56:13 +0200

    0

--- News_Project/js/main.js
+++ News_Project/js/main.js
@@ -251,26 +251,26 @@ function click_language() {
   console.log("value", leng);
 }
 
-// var unirest = require("unirest");
+//var unirest = require("unirest");
 
-// var req = unirest("GET", "https://community-open-weather-map.p.rapidapi.com/weather");
+var req = unirest("GET", "https://community-open-weather-map.p.rapidapi.com/weather");
 
-// req.query({
-//   "callback": "test",
-//   "id": "2172797",
-//   "units": "%22metric%22 or %22imperial%22",
-//   "mode": "xml%2C html",
-//   "q": "London%2Cuk"
-// });
+req.query({
+  "callback": "test",
+  "id": "2172797",
+  "units": "%22metric%22 or %22imperial%22",
+  "mode": "xml%2C html",
+  "q": "London%2Cuk"
+});
 
-// req.headers({
-//   "x-rapidapi-host": "community-open-weather-map.p.rapidapi.com",
-//   "x-rapidapi-key": "a179aa314cmsh8046c773dd8f1edp15a74cjsne9d18d208bee"
-// });
+req.headers({
+  "x-rapidapi-host": "community-open-weather-map.p.rapidapi.com",
+  "x-rapidapi-key": "a179aa314cmsh8046c773dd8f1edp15a74cjsne9d18d208bee"
+});
 
 
-// req.end(function (res) {
-//   if (res.error) throw new Error(res.error);
+req.end(function (res) {
+  if (res.error) throw new Error(res.error);
 
-//   console.log(res.body);
-// });
+  console.log(res.body);
+});
